2947a874a00d94afb3a7a8f3671e829065ecbfcd
Muhammad Ghazian <mghazian@github.com>
2025-09-25 12:42:49 +0700

    feat: Add base authentication configuration

---
+++ src/main/java/com/kopi/belimang/auth/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.kopi.belimang.auth;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .formLogin(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(registry -> {
+                    registry.anyRequest().permitAll();
+                });
+        return httpSecurity.build();
+    }
+}
