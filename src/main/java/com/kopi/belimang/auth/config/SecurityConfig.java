8f8d2b65b8812850c60f3ac31cced4e331ecb982
Muhammad Ghazian <mghazian@github.com>
2025-10-06 20:13:49 +0700

    fix: Make sure unauthorized entry receive 401

--- src/main/java/com/kopi/belimang/auth/config/SecurityConfig.java
+++ src/main/java/com/kopi/belimang/auth/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.kopi.belimang.auth.config;
 
 
+import com.kopi.belimang.auth.core.AuthenticationEntryPointImpl;
 import com.kopi.belimang.auth.core.guard.GuardRegistry;
 import com.kopi.belimang.auth.core.CustomUserDetailService;
 import com.kopi.belimang.auth.core.JwtAuthenticationFilter;
@@ -22,10 +23,12 @@ public class SecurityConfig {
 
     private final GuardRegistry guardRegistry;
     private final CustomUserDetailService customUserDetailService;
+    private final AuthenticationEntryPointImpl customEntryPoint;
 
-    public SecurityConfig(GuardRegistry guardRegistry, CustomUserDetailService customUserDetailService) {
+    public SecurityConfig(GuardRegistry guardRegistry, CustomUserDetailService customUserDetailService, AuthenticationEntryPointImpl customEntryPoint) {
         this.guardRegistry = guardRegistry;
         this.customUserDetailService = customUserDetailService;
+        this.customEntryPoint = customEntryPoint;
     }
 
     @Bean
@@ -42,6 +45,7 @@ public class SecurityConfig {
                     registry.requestMatchers("user/**").permitAll();
                 })
                 .authenticationProvider(daoAuthenticationProvider())
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(customEntryPoint))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
